ffb9ab14950c150123f92bf48a42f8732b76cf4d
Jean Matheus <jeanmatheus0612@gmail.com>
2026-05-27 21:12:50 -0300

    Agrobot API

---
+++ src/main/java/com/fiap/agrobot/AgrobotApplication.java
@@ -0,0 +1,13 @@
+package com.fiap.agrobot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgrobotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgrobotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/agrobot/AgrobotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap.agrobot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrobotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
