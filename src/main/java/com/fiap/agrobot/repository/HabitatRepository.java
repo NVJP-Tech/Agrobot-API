0a4df9f6690d9253f43136bea1118a915e627180
Jean Matheus <jeanmatheus0612@gmail.com>
2026-05-29 01:14:04 -0300

    feat: integra banco oracle e implementa monitoramento real de habitats

---
+++ src/main/java/com/fiap/agrobot/repository/HabitatRepository.java
@@ -0,0 +1,7 @@
+package com.fiap.agrobot.repository;
+
+import com.fiap.agrobot.model.Habitat;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HabitatRepository extends JpaRepository<Habitat, Long> {
+}
